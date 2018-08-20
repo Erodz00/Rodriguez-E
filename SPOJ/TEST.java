81c97a77d84163c2726fcb05c0cf5313a6f13514
Enrique Rodriguez <soraestafar@gmail.com>
2018-08-20 11:54:49 -0400

    Create TEST.java

---
+++ SPOJ/TEST.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Life{
+    public static void main(String args[]){
+        Scanner scan =new Scanner(System.in);
+        while(true){
+            int n=scan.nextInt();    
+            if(n == 42)break;
+            System.out.println(n); 
+        }
+    }
+}
